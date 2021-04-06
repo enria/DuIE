0d00ebac733d096750eea417b7cc55d5de73e93c
Zhang Yadong <51205901050@stu.ecnu.edu.cn>
2021-04-06 21:46:57 +0800

    First init

---
+++ fin_EE/fin_EE_data_util.py
@@ -0,0 +1,207 @@
+import sys
+import os
+
+# 添加src目录
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(BASE_DIR))
+
+from transformers import (
+    DataProcessor,
+    BertTokenizerFast
+)
+from collections import defaultdict
+import torch
+import csv
+import numpy as np
+import json
+
+surpass_role_cnt=0
+overlap_role_cnt=0
+
+
+class NERProcessor(DataProcessor):
+    """
+        从NER数据文件/data/NER_data下读取数据，生成NER训练数据dataloader，返回给模型
+    """
+
+    def __init__(self, config, tokenizer=None):
+        self.train_path = config.train_path
+        self.dev_path = config.dev_path
+        self.test_path=config.test_path
+        self.tokenizer = BertTokenizerFast.from_pretrained(config.pretrained_path)
+        self.event_schema=EventSchemaDict(config.schema_path)
+
+    def read_json_data(self, filename):
+        data = []
+        with open(filename, 'r') as fjson:
+            for line in fjson:
+                item = json.loads(line)
+                data.append(item)
+        return data
+
+    def get_train_data(self):
+        data = self.read_json_data(self.train_path)
+        return data
+    
+    def get_dev_data(self):
+        data = self.read_json_data(self.dev_path)
+        return data
+    
+    def get_test_data(self):
+        test_data = self.read_json_data(self.test_path)
+        return test_data
+
+    def from_offset_to_label_index_list(self,offset,label_index):
+        offset_index=1
+        label_index_list=[0]*len(offset)
+        for role_index in label_index:
+            if role_index[0][0]<offset[offset_index][0]:
+                continue
+            while offset_index<len(offset):
+                if offset[offset_index][0]>=role_index[0][0]:
+                    label_index_list[offset_index]=role_index[1][0]
+                    break
+                offset_index+=1
+
+            offset_index+=1
+            while offset_index<len(offset):
+                if offset[offset_index][1]<=role_index[0][1]:
+                    label_index_list[offset_index]=role_index[1][1]
+                    offset_index+=1
+                else:
+                    break
+            
+            if offset_index>=len(offset):
+                break
+        else:
+            pass #  TODO 统计覆盖
+        return label_index_list
+
+    def create_dataloader(self, data, batch_size, shuffle=False, max_length=512):
+        tokenizer = self.tokenizer
+
+        # 2. 分别对句子和公司进行编码表示
+        text = [d["text"] for d in data]
+        max_length = min(max_length, max([len(s) for s in text]))
+        print("max sentence length: ", max_length)
+
+        inputs = tokenizer(     # 得到文本的编码表示（句子前后会加入<cls>和<sep>特殊字符，并且将句子统一补充到最大句子长度
+            text,
+            padding=True,
+            return_tensors="pt",
+            max_length=max_length,
+            truncation=True,
+            return_offsets_mapping=True
+        )
+
+        # 3. 在句子中查找公司，得到句子的标注BIO编码 labels：将公司名标注为B和I, 非公司名字段标注为O
+        labels = []
+        for index,offset in enumerate(inputs['offset_mapping']):
+            event_list=data[index].get("event_list",[])
+            label_index=self.event_schema.tokens_to_label_index(data[index]["text"],event_list)
+            label_index_list=self.from_offset_to_label_index_list(offset,label_index)
+            labels.append(label_index_list)
+
+        # 4. 将得到的句子编码和BIO转为dataloader，供模型使用
+        dataset = torch.utils.data.TensorDataset(
+            torch.LongTensor(inputs["input_ids"]),          # 句子字符id
+            torch.LongTensor(inputs["token_type_ids"]),     # 区分两句话，此任务中全为0，表示输入只有一句话
+            torch.LongTensor(inputs["attention_mask"]),     # 区分是否是pad值。句子内容为1，pad为0
+            torch.LongTensor(inputs["offset_mapping"]),     # 区分是否是pad值。句子内容为1，pad为0
+            torch.LongTensor(labels),
+        )
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            shuffle=shuffle,
+            batch_size=batch_size,
+            num_workers=4,
+        )
+        return dataloader
+
+class EventSchemaDict(object):
+    def __init__(self,schema_path):
+        self.schema_path=schema_path
+        self.label2ids={}
+        self.id2labels={}
+
+        self.load_schema()
+
+    def load_schema(self):
+        role_list=[]
+        with open(self.schema_path) as schema:
+            for line in schema:
+                if not line:
+                    continue
+                event_schema=json.loads(line)
+                event_type=event_schema["event_type"]
+                for role in event_schema["role_list"]:
+                    role_list.append(event_type+":"+role["role"])
+
+        self.label2ids["O"]=0
+        self.id2labels[0]="O"
+        for index,role in enumerate(role_list):
+            self.label2ids["B-"+role]=index*2+1
+            self.label2ids["I-"+role]=index*2+2
+            self.id2labels[index*2+1]="B-"+role
+            self.id2labels[index*2+2]="I-"+role
+    
+    def tokens_to_label_index(self,text:str,label):
+        label_index=[]
+        for event_index,event in enumerate(label):
+            event_type=event["event_type"]
+            for role in event["arguments"]:
+                role_start=text.find(role["argument"])
+                role_end=role_start+len(role["argument"])
+                B_label_id=self.label2ids["B-"+event_type+":"+role["role"]]
+                I_label_id=self.label2ids["I-"+event_type+":"+role["role"]]
+                label_index.append(((role_start,role_end),(B_label_id,I_label_id),event_index))
+        label_index.sort(key=lambda x:(x[0][0],x[2]))
+        return label_index
+    
+
+
+
+if __name__ == '__main__':
+
+    from collections import namedtuple
+
+    config_class = namedtuple('config',['train_path','dev_path','test_path','pretrained_path','schema_path'])
+    config = config_class("../data/train/duee_fin_train.json","../data/dev/duee_fin_dev.json","../data/test/duee_fin_test1.json","../pretrain/bert-base-chinese","../data/schema/duee_fin_event_schema.json")
+
+    processor=NERProcessor(config)
+    train_data=processor.get_train_data()
+
+    text_length=[len(item["text"]) for item in train_data]
+
+    exceed=list(filter(lambda x: x>=550,text_length))
+
+    print(len(exceed)/len(text_length))
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import matplotlib
+
+    # 设置matplotlib正常显示中文和负号
+    matplotlib.rcParams['font.sans-serif']=['SimHei']   # 用黑体显示中文
+    matplotlib.rcParams['axes.unicode_minus']=False     # 正常显示负号
+    # 随机生成（10000,）服从正态分布的数据
+    data = np.random.randn(10000)
+    """
+    绘制直方图
+    data:必选参数，绘图数据
+    bins:直方图的长条形数目，可选项，默认为10
+    normed:是否将得到的直方图向量归一化，可选项，默认为0，代表不归一化，显示频数。normed=1，表示归一化，显示频率。
+    facecolor:长条形的颜色
+    edgecolor:长条形边框的颜色
+    alpha:透明度
+    """
+    plt.hist(text_length, bins=40, facecolor="blue", edgecolor="black", alpha=0.7)
+    # 显示横轴标签
+    plt.xlabel("区间")
+    # 显示纵轴标签
+    plt.ylabel("频数/频率")
+    # 显示图标题
+    plt.title("频数/频率分布直方图")
+    # plt.show()
+    plt.savefig("hist.png")
+        
---
+++ fin_EE/fin_EE_model.py
@@ -0,0 +1,262 @@
+# coding=utf-8
+from fin_EE_data_util import NERProcessor
+import pytorch_lightning as pl
+# from sklearn import model_selection
+import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchcrf import CRF
+import csv
+from conlleval import evaluate
+from transformers import (
+    BertTokenizerFast,
+    BertForTokenClassification,
+    BertModel
+)
+import numpy as np
+import re
+import json
+# from model_layers import LSTM_attention, multihead_attention
+
+# from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+class NERModel(pl.LightningModule):
+    def __init__(self, config):
+        # 1. Init parameters
+        super(NERModel, self).__init__()
+        
+
+        self.batch_size = config.batch_size
+        self.lr = config.lr
+        self.crf_lr = config.crf_lr
+        self.dropout = config.dropout
+        self.optimizer = config.optimizer
+
+        self.use_bert = config.use_bert
+        self.use_crf = config.use_crf
+        
+        self.layerNorm = torch.nn.LayerNorm
+        # 2. Init Model
+        if self.use_bert: # BERT模型
+            self.tokenizer = BertTokenizerFast.from_pretrained(config.pretrained_path)  # 
+            special_tokens_dict = { 'additional_special_tokens': ["<COM>", "“", "”"] }  # 在词典中增加特殊字符
+            self.tokenizer.add_special_tokens(special_tokens_dict)
+            self.processor = NERProcessor(config, self.tokenizer)
+
+            self.labels = len(self.processor.event_schema.id2labels)
+
+            self.model = BertForTokenClassification.from_pretrained(
+                config.pretrained_path, num_labels=self.labels)
+            self.model.resize_token_embeddings(len(self.tokenizer))
+            print('num tokens:', len(self.tokenizer))
+
+        # 2. Init crf model and loss
+        if self.use_crf:
+            # crf doc: https://pytorch-crf.readthedocs.io/en/stable/
+            self.crf = CRF(num_tags=self.labels, batch_first=True)
+        else:
+            # weight = torch.Tensor([1, 100, 50])
+            # self.criterion = nn.CrossEntropyLoss(weight=weight)
+            self.criterion = nn.CrossEntropyLoss()
+            # self.criterion = nn.NLLLoss(ignore_index=0, size_average=True)
+
+        print("NER model init: done.")
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None):
+        if self.use_bert:
+            feats = self.model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+            )[0]  # [bs,len,numlabels]
+
+        return feats
+
+    def training_step(self, batch, batch_idx):
+        if self.use_bert:
+            input_ids, token_type_ids, attention_mask,offset_mapping, labels = batch
+            feats = self(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+
+        if self.use_crf:
+            loss = -self.crf(feats, labels,
+                             mask=attention_mask.byte(), reduction='mean')
+            # pred = self.crf.decode(feats)
+            # acc = (torch.tensor(pred).cuda() == labels).float().mean()
+        else:
+            loss = self.criterion(feats[attention_mask!=0,:], labels[attention_mask!=0])
+            # pred = feats.argmax(dim=-1)
+            # acc = (pred == labels).float().mean()
+
+        # tensorboard_logs = {'train_loss': loss}
+        # return {'loss': loss, 'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
+        self.log('train_loss', loss)
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+        if self.use_bert:
+            input_ids, token_type_ids, attention_mask,offset_mapping, labels = batch
+            feats = self(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+
+        if self.use_crf:
+            loss = -self.crf(feats, labels,
+                             mask=attention_mask.byte(), reduction='mean')
+            pred = self.crf.decode(feats)
+            # acc = (torch.tensor(pred).cuda() == labels).float().mean()
+        else:
+            loss = self.criterion(feats[attention_mask!=0,:], labels[attention_mask!=0])
+            pred = feats.argmax(dim=-1)
+            # acc = (pred == labels).float().mean()
+
+        gold = labels[attention_mask != 0]
+        pre = torch.tensor(pred).cuda()[attention_mask != 0]
+
+        # return {'val_loss': loss, 'gold': gold, 'pre': pre}
+        return loss,gold,pre
+
+    def prepare_data(self):
+        train_data = self.processor.get_train_data()
+        val_data=self.processor.get_dev_data()
+
+        print("train_length:", len(train_data))
+        print("valid_length:", len(val_data))
+
+        if self.use_bert:
+            self.train_loader = self.processor.create_dataloader(
+                train_data, batch_size=self.batch_size, shuffle=True)
+            self.valid_loader = self.processor.create_dataloader(
+                val_data, batch_size=self.batch_size, shuffle=False)
+            # self.test_loader = self.processor.create_dataloader(val_data, batch_size=self.batch_size, shuffle=False)
+        else:
+            # self.processor.get_embedding(train_data)
+            # self.embeds = nn.Embedding.from_pretrained(torch.FloatTensor(self.processor.wordEmbedding), freeze=False)
+            self.train_loader = self.processor.create_dataloader_with_pretrain_embedding(
+                train_data, batch_size=self.batch_size, shuffle=True)
+            self.valid_loader = self.processor.create_dataloader_with_pretrain_embedding(
+                val_data, batch_size=self.batch_size, shuffle=False)
+
+    def validation_epoch_end(self, outputs):
+
+        val_loss,gold,pre=zip(*outputs)
+        
+        val_loss = torch.stack(val_loss).mean().cpu()
+        gold = torch.cat(gold).cpu()
+        pre = torch.cat(pre).cpu()
+
+        true_seqs = [self.processor.event_schema.id2labels[int(g)] for g in gold]
+        pred_seqs = [self.processor.event_schema.id2labels[int(g)] for g in pre]
+
+        print('\n')
+        prec, rec, f1 = evaluate(true_seqs, pred_seqs)
+        # tensorboard_logs = {'val_loss': val_loss, 'val_pre': torch.tensor(prec),
+        #                     'val_rec': rec, 'val_f1': torch.tensor(f1)}
+        # return {'val_loss': val_loss, 'log': tensorboard_logs, 'progress_bar': tensorboard_logs}
+
+        self.log('val_loss', val_loss)
+        self.log('val_pre', torch.tensor(prec))
+        self.log('val_rec', rec)
+        self.log('val_f1', torch.tensor(f1))
+
+    def configure_optimizers(self):
+        if self.use_crf:
+            crf_params_ids = list(map(id, self.crf.parameters()))
+            base_params = filter(lambda p: id(p) not in crf_params_ids, [
+                                 p for p in self.parameters() if p.requires_grad])
+
+            arg_list = [{'params': base_params}, {'params': self.crf.parameters(), 'lr': self.crf_lr}]
+        else:
+            # label_embed_and_attention_params = list(map(id, self.label_embedding.parameters())) + list(map(id, self.self_attention.parameters()))
+            # arg_list = [{'params': list(self.label_embedding.parameters()) + list(self.self_attention.parameters()), 'lr': self.lr}]
+            arg_list = [p for p in self.parameters() if p.requires_grad]
+
+        print("Num parameters:", len(arg_list))
+        if self.optimizer == 'Adam':
+            return torch.optim.Adam(arg_list, lr=self.lr, eps=1e-8)
+        elif self.optimizer == 'SGD':
+            return torch.optim.SGD(arg_list, lr=self.lr, momentum=0.9)
+
+    def train_dataloader(self):
+        return self.train_loader
+
+    def val_dataloader(self):
+        return self.valid_loader
+
+
+class NERPredictor:
+    def __init__(self, checkpoint_path, config):
+        self.use_bert = config.use_bert
+        self.use_crf = config.use_crf
+
+        self.model = NERModel.load_from_checkpoint(checkpoint_path, config=config)
+
+        self.test_data = self.model.processor.get_test_data()
+        if config.use_bert:
+            self.tokenizer = self.model.tokenizer
+            self.dataloader = self.model.processor.create_dataloader(
+                self.test_data, batch_size=config.batch_size, shuffle=False)
+
+
+        print("The TEST num is:", len(self.test_data))
+        print('load checkpoint:', checkpoint_path)
+
+    def extract_events(self,text,offset_mapping,preds):
+        events_dict={}
+        index=1 #直接跳过CLS
+        last_B,last_B_index=0,0
+        while index<len(preds):
+            if last_B:
+                if preds[index]!=last_B+1:
+                    argument=text[offset_mapping[last_B_index][0]:offset_mapping[index-1][1]]
+                    label=self.model.processor.event_schema.id2labels[last_B]
+                    label=re.match("B-(.+):(.+)",label).groups()
+                    events_dict.setdefault(label[0],[])
+                    events_dict[label[0]].append((label[1],argument))
+                    
+                    last_B,last_B_index=0,0
+
+            if preds[index]!=0 and preds[index]%2:
+                last_B,last_B_index=preds[index],index
+            index+=1
+        events=[]
+        for event_type,roles in events_dict.items():
+            events.append({
+                "event_type":event_type,
+                "arguments":[{
+                    "role":role[0],
+                    "argument":role[1]
+                } for role in roles]
+            })
+        return events
+
+
+    def generate_result(self, outfile_txt):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model.to(device)
+        self.model.eval()
+
+        cnt = 0
+
+        with open(outfile_txt, 'w') as fout:
+            for batch in tqdm.tqdm(self.dataloader):
+                for i in range(len(batch)):
+                    batch[i] = batch[i].to(device)
+                if self.use_bert:
+                    input_ids, token_type_ids, attention_mask,offset_mapping,labels = batch
+
+                emissions = self.model(input_ids, token_type_ids, attention_mask)
+
+                if self.use_crf:
+                    preds = self.model.crf.decode(emissions)  # list
+                else:
+                    preds = emissions.argmax(dim=-1).cpu()
+
+
+                for offset,pred in zip(offset_mapping,preds):
+                    item=dict(self.test_data[cnt].items())
+                    events=self.extract_events(item["text"],offset,pred)
+                    item["event_list"]=events
+            
+                    fout.write(json.dumps(item,ensure_ascii=False)+"\n")
+                    cnt+=1
+
+        print('done--all %d tokens.' % cnt)
